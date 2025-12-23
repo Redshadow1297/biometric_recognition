b4afe94b25f5ad5127e58b28aff6ff4db6b38d4d
Amol Shinde <amol.d.shinde01@gmail.com>
2025-12-23 11:55:09 +0530

    Demo project to get authenticate using the biometrics

---
+++ android/app/src/main/kotlin/com/example/authentication_fingerprint_faceid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authentication_fingerprint_faceid
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {
+}
---
+++ lib/biometric_auth_demo.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+
+class BiometricAuthDemo extends StatefulWidget {
+  const BiometricAuthDemo({super.key});
+
+  @override
+  State<BiometricAuthDemo> createState() => _BiometricAuthDemoState();
+}
+
+class _BiometricAuthDemoState extends State<BiometricAuthDemo> {
+  final LocalAuthentication auth = LocalAuthentication();
+  String authStatus = "Not Authenticated";
+
+   Future<void> authenticateUser() async {
+    try {
+      final canCheck = await auth.canCheckBiometrics;
+      final isSupported = await auth.isDeviceSupported();
+      final biometrics = await auth.getAvailableBiometrics();
+
+      if (!canCheck || !isSupported || biometrics.isEmpty) {
+        setState(() {
+          authStatus = "No biometric or FaceId authentication available";
+        });
+        return;
+      }
+
+      String biometricType = "Biometric";
+
+      if (biometrics.contains(BiometricType.iris)) {               //iris scans the eyes and get authenticated for both face and fingerprint
+        biometricType = "Iris Authentication";
+      }
+      // if (biometrics.contains(BiometricType.face)) {   //Fingerprint Authentication
+      //   biometricType = "Face ID Authentication";
+      // }
+      // else if (biometrics.contains(BiometricType.fingerprint)) {   //Fingerprint Authentication
+      //   biometricType = "Fingerprint Authentication";
+      // }
+
+      final authenticated = await auth.authenticate(
+        localizedReason: 'Authenticate using $biometricType',
+        options: const AuthenticationOptions(
+          biometricOnly: false,
+          stickyAuth: true,
+          useErrorDialogs: true,
+        ),
+      );
+
+      setState(() {
+        authStatus = authenticated
+            ? "$biometricType Successful"
+            : "$biometricType Failed";
+      });
+    } catch (e) {
+      setState(() {
+        authStatus = "Authentication error";
+      });
+      debugPrint("AUTH ERROR: $e");
+    }
+  }
+
+  //Only using Face ID
+  // Future<void> authenticateFaceOnly() async {
+  //   try {
+  //     final biometrics = await auth.getAvailableBiometrics();
+
+  //     if (!biometrics.contains(BiometricType.face)) {
+  //       setState(() {
+  //         authStatus = "Face ID not available on this device";
+  //       });
+  //       return;
+  //     }
+  //     // Call authenticate() → will use Face ID because only Face is available/enrolled
+  //     final authenticated = await auth.authenticate(
+  //       localizedReason: 'Authenticate using Face ID',
+  //       options: const AuthenticationOptions(
+  //         biometricOnly: true,
+  //         stickyAuth: true,
+  //         useErrorDialogs: true,
+  //       ),
+  //     );
+
+  //     setState(() {
+  //       authStatus = authenticated ? "Face ID Successful" : "Face ID Failed";
+  //     });
+  //   } catch (e) {
+  //     setState(() {
+  //       authStatus = "Authentication error: $e";
+  //     });
+  //     debugPrint("AUTH ERROR: $e");
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Biometric Auth Demo")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(authStatus, style: const TextStyle(fontSize: 18)),
+            const SizedBox(height: 30),
+            ElevatedButton.icon(
+              onPressed: authenticateUser,
+              // onPressed: authenticateFaceOnly,
+              icon: biometricIcon([]) != null
+                  ? Icon(biometricIcon([]))
+                  : const Icon(Icons.lock),
+              label: const Text("Authenticate"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  IconData biometricIcon(List<BiometricType> biometrics) {
+    if (biometrics.contains(BiometricType.face)) {
+      return Icons.face;
+    }
+    return Icons.fingerprint;
+  }
+}
